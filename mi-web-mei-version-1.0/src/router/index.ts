fdfff7e84b713f92b8981cb13534f69c1616453a
Elena <elenapinvoz@gmail.com>
2026-03-10 17:03:57 +0100

    Resolución imagenes bien

--- mi-web-mei-version-1.0/src/router/index.ts
+++ mi-web-mei-version-1.0/src/router/index.ts
@@ -2,6 +2,7 @@ import { createRouter, createWebHashHistory } from "vue-router";
 import Home from "@/pages/home/Home.vue";
 import SobreMi from "@/pages/sobremi/sobremi.vue";
 import Proyectos from "@/pages/proyectos/proyectos.vue";
+import ProyectosLayout from "@/pages/proyectos/ProyectosLayout.vue";
 import Contacto from "@/pages/contacto/contacto.vue";
 import ProyectoKaoka from "@/pages/proyectos/Kaoka.vue";
 import ProyectoMindPop from "@/pages/proyectos/Mindpop.vue";
@@ -15,13 +16,19 @@ export const router = createRouter({
   routes: [
     { path: "/", name: "home", component: Home },
     { path: "/sobremi", name: "sobremi", component: SobreMi },
-    { path: "/proyectos", name: "proyectos", component: Proyectos },
-    { path: "/proyectos/Kaoka", name: "proyecto-kaoka", component: ProyectoKaoka },
-    { path: "/proyectos/Mindpop", name: "proyecto-mind-pop", component: ProyectoMindPop },
-    { path: "/proyectos/Crefad", name: "proyecto-crefad", component: ProyectoCrefad },
-    { path: "/proyectos/Comic", name: "proyecto-comic", component: ProyectoComic },
-    { path: "/proyectos/Lavejez", name: "proyecto-lavejez", component: ProyectoLavejez },
-    { path: "/proyectos/Loquenosmueve", name: "proyecto-loquenosmueve", component: ProyectoLoquenosmueve },
+    {
+      path: "/proyectos",
+      component: ProyectosLayout,
+      children: [
+        { path: "", name: "proyectos", component: Proyectos },
+        { path: "Kaoka", name: "proyecto-kaoka", component: ProyectoKaoka },
+        { path: "Mindpop", name: "proyecto-mind-pop", component: ProyectoMindPop },
+        { path: "Crefad", name: "proyecto-crefad", component: ProyectoCrefad },
+        { path: "Comic", name: "proyecto-comic", component: ProyectoComic },
+        { path: "Lavejez", name: "proyecto-lavejez", component: ProyectoLavejez },
+        { path: "Loquenosmueve", name: "proyecto-loquenosmueve", component: ProyectoLoquenosmueve },
+      ],
+    },
     { path: "/contacto", name: "contacto", component: Contacto },
     { path: "/:pathMatch(.*)*", redirect: "/" },
   ],
